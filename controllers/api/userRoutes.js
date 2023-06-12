0e6d17b1da5bff5acc12cae8b1d482c6f8b3ffef
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-06-12 18:58:57 -0400

    added the home routes to our file

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -15,7 +15,7 @@ router.post("/", async (req, res) => {
 });
 router.post("/login", async (req, res) => {
   try {
-    const userData = await User.findOne({ where: req.body.email });
+    const userData = await User.findOne({ where: {email: req.body.email} });
     if (!userData) {
       res
         .status(400)
