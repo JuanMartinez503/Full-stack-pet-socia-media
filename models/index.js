017525f12e452a5b2a7b2d0fdbe1aba5f75ad129
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-06-13 13:00:47 -0400

    forgot to push the other document

--- models/index.js
+++ models/index.js
@@ -15,4 +15,14 @@ User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 })
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+})
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+})
+module.exports = { User, Post, Comment};
