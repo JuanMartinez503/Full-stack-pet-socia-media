97bf1cd0696bcf9e8533923b9f30933ec0045c0c
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-06-15 13:59:44 -0400

    I connected tailwind to our project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-  content: [
-    './views/*/*.handlebars',
-    './views/*.handlebars',
-    './public/*/*.js'
-  ],
+  content: ["./views/**/*.handlebars"],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      neutral: colors.neutral,
-      orange: colors.orange,
-      lime: colors.lime,
-      cyan: colors.cyan,
-      indigo: colors.indigo
-    },
-    fontFamily: {
-      sans: ['Century Gothic', 'CenturyGothic', 'AppleGothic', 'sans-serif'],
-      serif: ['Didot', 'Didot LT STD', 'Hoefler Text', 'Garamond', 'Calisto MT', 'Times New Roman', 'serif']
-    },
     extend: {},
   },
   plugins: [],
